cb7b8f88095294ea86c83e8cda7a637c5b75d84d
Nguyễn Đình Trường <53716352+ndtr2000@users.noreply.github.com>
2021-07-03 19:40:59 +0700

    Update User.java

--- app/src/main/java/com/example/helpvnwin/models/User.java
+++ app/src/main/java/com/example/helpvnwin/models/User.java
@@ -1,29 +1,25 @@
 package com.example.helpvnwin.models;
 import java.util.List;
-import java.util.Vector;
+
 
 
 public class User {
     private String id;
     private String username;
     private String imageURL;
-    private AES engine;
-    private String box;
+
 
 
     public User(String id, String username, String imageURL) {
         this.id = id;
         this.username = username;
         this.imageURL = imageURL;
-        this.engine = new AES();
-    }
-    public User(String username){
-        this.username = username;
-        this.engine = new AES();
+
     }
 
+
     public User(){
-        this.engine = new AES();
+
     }
 
     public String getId() {
@@ -49,9 +45,5 @@ public class User {
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
-    public void forward(String message, User receiver){
-        String log = String.format("Forward %s from %s to %s", message, this.username, receiver.username);
-        receiver.box = message;
-        System.err.println(log);
-    }
+
 }
