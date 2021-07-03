e9c719dc95929597da6907c68f0edfcd79ac581a
Nguyễn Đình Trường <53716352+ndtr2000@users.noreply.github.com>
2021-07-03 19:44:37 +0700

    Update User.java

--- app/src/main/java/com/example/helpvnwin/models/User.java
+++ app/src/main/java/com/example/helpvnwin/models/User.java
@@ -1,26 +1,19 @@
 package com.example.helpvnwin.models;
-import java.util.List;
-
-
 
 public class User {
     private String id;
     private String username;
     private String imageURL;
+    private String status;
 
-
-
-    public User(String id, String username, String imageURL) {
+    public User(String id, String username, String imageURL, String status) {
         this.id = id;
+        this.status = status;
         this.username = username;
         this.imageURL = imageURL;
-
     }
 
-
-    public User(){
-
-    }
+    public User(){}
 
     public String getId() {
         return id;
@@ -46,4 +39,11 @@ public class User {
         this.imageURL = imageURL;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
