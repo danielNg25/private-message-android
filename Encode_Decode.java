709f561a7d02fed08538f44ee9c21ab215ca9cd3
hoangIT1 <55548345+hoangIT1@users.noreply.github.com>
2021-07-11 23:19:12 +0700

    Update Encode_Decode

--- Encode_Decode.java
+++ Encode_Decode.java
@@ -1,44 +1,55 @@
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.Vector;
 
-import javax.crypto.BadPaddingException;
+
+package aes;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Base64;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+
 
 public class Encode_Decode {
-	
-	private static String TRANS_MODE = "Blowfish";
-    private static String BLOWFISH_KEY = "BLOWFISH_KEY";
-	
-    public Encode_Decode() {
-    	super();
-    }
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-    public String encrypt(String password) throws IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
-    	SecretKeySpec keySpec = new SecretKeySpec(BLOWFISH_KEY.getBytes("Windows-31J"),TRANS_MODE);
-    	Cipher cipher;
-        cipher = Cipher.getInstance(TRANS_MODE);
-
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-        byte[] passByte;
-        passByte = cipher.doFinal(password.getBytes("Windows-31J"));
-
-        return new String(bytesToHex(passByte));
-    }
     
+    
+    public String encrypt(String strToEncrypt, String myKey, String name) {
+      try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            byte[] key = myKey.getBytes("UTF-8");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(name.getBytes("UTF-8"))) + ":" + Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+      } catch (Exception e) {
+            System.out.println(e.toString());
+      }
+      return null;
+    }
+    public String[] decrypt(String str_nameToDecrypt, String myKey) {
+      try {
+    	  	String[] output = new String[2];
+    	  	String parts[] = str_nameToDecrypt.split(":");
+    	  	String name = parts[0];
+    	  	String strToDecrypt = parts[1];
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            byte[] key = myKey.getBytes("UTF-8");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            output[1] = new String(cipher.doFinal(Base64.getDecoder().decode(name)));
+            output[2] = new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+            return output;
+      } catch (Exception e) {
+            System.out.println(e.toString());
+      }
+      return null;
+    }   
 }
-
